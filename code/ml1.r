3652038c8efa83e9783c92d20ee3ad5f6659b136
Jared P. Lander <jared@landeranalytics.com>
2020-10-27 13:42:45 -0400

    Showed first recipe

--- code/ml1.r
+++ code/ml1.r
@@ -62,3 +62,45 @@ test <- testing(credit_split)
 train
 train %>% glimpse()
 train %>% class()
+
+library(skimr)
+skim(train)
+
+# Feature Engineering ####
+
+# from recipes
+
+# caret
+# tidymodels
+# food themes
+# Max Kuhn
+
+# Outcomes: response, y, label, target, dependent variable, output, known, result
+# inputs: predictors, x, features, covariates, independent variable, data, attributes, descriptors
+
+table(credit_data$Status)
+
+# red, green, blue
+colors <- c('blue', 'blue', 'red', 'green', 'blue', 'green', 'blue', 'blue', 'blue', 'blue',
+            'red', 'green', 'pink', 'yellow', 'pink', 'purple')
+model.matrix(~ colors)
+colors2 <- c('blue', 'blue', 'red', 'green', 'blue', 'green', 'blue', 'blue', 'blue', 'blue',
+            'red', 'green', 'Misc', 'Misc', 'Misc', 'Misc')
+model.matrix(~colors2)
+
+rec1 <- recipe(Status ~ ., data=train) %>% 
+    # xgboost can handle this, so we'll remove it later
+    step_downsample(Status, under_ratio=1.2) %>% 
+    # not really needed for xgboost
+    step_normalize(Age, Price) %>% 
+    # collapse infrequent columns in categorical variables
+    # step_other(Home, Marital, Records, Job, other='Misc')
+    # this line is a shortcut for the line above
+    step_other(all_nominal(), -Status, other='Misc') %>% 
+    # remove columns with very little variability, nero-zero variance
+    step_nzv(all_predictors()) %>% 
+    # xgboost doesn't need imputation, so we will remove later
+    step_modeimpute(all_nominal(), -Status) %>% 
+    step_knnimpute(all_numeric()) %>% 
+    step_dummy(all_nominal(), -Status, one_hot=TRUE)
+rec1
