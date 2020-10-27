82f6c3a98a865adb9746f47a4f6c5cd8ac53a4bc
Jared P. Lander <jared@landeranalytics.com>
2020-10-27 16:30:29 -0400

    Showed xgboost handling missing data

--- code/ml1.r
+++ code/ml1.r
@@ -233,3 +233,57 @@ cv1$.metrics[[3]]
 
 cv1 %>% collect_metrics()
 
+# More Parameters ####
+
+xg_spec2 <- boost_tree(mode='classification', trees=300) %>% 
+    set_engine('xgboost')
+xg_spec2
+
+# workflow() %>% 
+#     add_model(xg_spec2) %>% 
+#     add_recipe(rec1)
+
+flow2 <- flow1 %>% 
+    update_model(xg_spec2)
+flow2
+
+val2 <- fit_resamples(flow2, resamples=val_split, metrics=loss_fn)
+val2
+val2 %>% collect_metrics()
+
+xg_spec3 <- boost_tree('classification', trees=300, learn_rate=0.2) %>% 
+    set_engine('xgboost')
+xg_spec3
+
+flow3 <- flow2 %>% 
+    update_model(xg_spec3)
+
+val3 <- fit_resamples(flow3, resamples=val_split, metrics=loss_fn)
+val3 %>% collect_metrics()
+
+xg_spec4 <- boost_tree('classification', trees=300, learn_rate=0.2, sample_size=0.5) %>% 
+    set_engine('xgboost')
+xg_spec4
+
+flow4 <- flow3 %>% 
+    update_model(xg_spec4)
+val4 <- fit_resamples(flow4, resamples=val_split, metrics=loss_fn)
+val4 %>% collect_metrics()
+
+# Missing Data ####
+
+rec2 <- recipe(Status ~ ., data=train) %>% 
+    step_nzv(all_predictors()) %>% 
+    step_other(all_nominal(), -Status, other='Misc') %>% 
+    themis::step_downsample(Status, under_ratio=1.2) %>% 
+    step_dummy(all_nominal(), -Status, one_hot=TRUE)
+rec2
+
+flow5 <- flow4 %>% 
+    update_recipe(rec2)
+flow5
+
+val5 <- fit_resamples(flow5, resamples=val_split, metrics=loss_fn)
+val5 %>% collect_metrics()
+val4 %>% collect_metrics()
+
