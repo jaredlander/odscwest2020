c6cb1429b9d74942b67dec13383cbea35e8f4aeb
Jared P. Lander <jared@landeranalytics.com>
2020-10-28 18:39:01 -0400

    Built API function to predict

---
+++ code/BuildAPIModel.r
@@ -0,0 +1,39 @@
+library(tidymodels)
+
+data(credit_data, package='modeldata')
+
+# build a feature engineering recipes
+# build a model specification
+# combine them into a workflow
+# fit a model on the data
+# save the trained model to disc
+
+rec0 <- recipe(Status ~ Income + Seniority + Records + Amount + Job, data=credit_data) %>% 
+    step_other(all_nominal(), -Status, other='Misc') %>% 
+    step_nzv(all_predictors()) %>% 
+    step_dummy(all_nominal(), -Status, one_hot=TRUE)
+
+spec0 <- boost_tree(
+    mode='classification'
+    , trees=53
+    , tree_depth=2
+    , learn_rate=0.2
+    , sample_size=0.52
+) %>% 
+    set_engine('xgboost')
+
+rec0
+spec0
+
+flow0 <- workflow() %>% 
+    add_recipe(rec0) %>% 
+    add_model(spec0)
+flow0
+
+mod0 <- fit(flow0, data=credit_data)
+mod0 %>% extract_model() %>% vip::vip()
+
+readr::write_rds(mod0, file='code/mod0.rds')
+
+loaded_mod <- readr::read_rds(file='code/mod0.rds')
+loaded_mod %>% extract_model() %>% vip::vip()
--- code/plumber.r
+++ code/plumber.r
@@ -23,3 +23,31 @@ function(x, y)
 {
     return(as.numeric(x) + as.numeric(y))
 }
+
+the_mod <- readr::read_rds(here::here('code', 'mod0.rds'))
+
+library(workflows)
+
+#* Predict Status from individual data
+#* @param Income:numeric hundreds
+#* @param Seniority singles
+#* @param Records "yes"/"no"
+#* @param Amount thousands
+#* @param Job "fixed"/"freelance"/"others"/"partime"
+#* @get /predict
+function(Income, Seniority, Records, Amount, Job)
+{
+    the_data <- data.frame(
+        Income=as.numeric(Income)
+        , Seniority=as.numeric(Seniority)
+        , Records=Records
+        , Amount=as.numeric(Amount)
+        , Job=Job
+    )
+    
+    return(
+        list(
+            predicted_status=predict(the_mod, new_data=the_data, type='prob')
+        )
+    )
+}
